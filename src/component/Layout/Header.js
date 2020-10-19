80eb2e7ad3888a2b81d90424476a6f6b31343389
Nabeel Qureshi <nabeel@blowoutandgo.com>
2020-10-19 18:53:53 +0400

    updating css

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     let {routes} = props
     return (
         <Navbar bg="light" expand="lg" className="headerBar">
-            <Navbar.Brand href="#home"><img src={logo} width="50%"/></Navbar.Brand>
+            <Navbar.Brand href="#home"><img className="headerImg" src={logo} width="50%"/></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
